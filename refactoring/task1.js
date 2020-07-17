dbcfcb43753cb81f6d58ec4ede1835eed4c99c80
Ivan Pankrashin <pankrashin.ivan@gmail.com>
2020-07-17 17:50:26 +0200

    refactoring task1

---
+++ refactoring/task1.js
@@ -0,0 +1,49 @@
+function func(s, a, b) {
+
+    if (s.match(/^$/)) {
+        return -1;
+    }
+
+    var i = s.length -1;
+    var aIndex =     -1;
+    var bIndex =     -1;
+
+    while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
+        if (s.substring(i, i +1) == a) {
+            aIndex = i;
+        }
+        if (s.substring(i, i +1) == b) {
+            bIndex = i;
+        }
+        i = i - 1;
+    }
+
+    if (aIndex != -1) {
+        if (bIndex == -1) {
+            return aIndex;
+        }
+        else {
+            return Math.max(aIndex, bIndex);
+        }
+    }
+
+    if (bIndex != -1) {
+        return bIndex;
+    }
+    else {
+        return -1;
+    }
+}
+
+function lastIndexOfChars(str, charA, charB) {
+    return Math.max(str.lastIndexOf(charA), str.lastIndexOf(charB));
+}
+
+console.log(lastIndexOfChars('', 'w', 'd'));
+console.log(func('', 'w', 'd'));
+
+console.log(lastIndexOfChars('hello World', 'w', 'd'));
+console.log(func('hello World', 'w', 'd'));
+
+console.log(lastIndexOfChars('hello World', 'ws', 'sd'));
+console.log(func('hello World', 'ws', 'sd'));
